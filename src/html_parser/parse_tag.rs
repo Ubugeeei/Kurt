cf4f9f3a495563523da5b60b7820292ea16ae9e7
koji_nishimura1 <koji_nishimura@mates-system.com>
2021-07-31 22:19:01 +0900

    tweak

--- src/html_parser/parse_tag.rs
+++ src/html_parser/parse_tag.rs
@@ -1,8 +1,9 @@
-#[allow(unused_imports)]
-use crate::html_parser::dom::{AttrMap, Element, Node, Text};
+use crate::html_parser::dom::{AttrMap};
 use combine::parser::char::char;
+
 #[allow(unused_imports)]
 use combine::EasyParser;
+
 use combine::{between, many, many1, Parser, Stream};
 use combine::{
     error::ParseError,
@@ -11,17 +12,63 @@ use combine::{
 
 use super::parse_attributes::attributes;
 
-pub fn open_tag<Input>() -> impl Parser<Input, Output = (String, AttrMap)>
+/**
+ * 開始タグ パース
+ */
+pub fn start_tag<Input>() -> impl Parser<Input, Output = (String, AttrMap)>
 where
     Input: Stream<Token = char>,
     Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
 {
-    let open_tag_name = many1::<String, _, _>(letter());
-    let open_tag_content = (
-        open_tag_name,
+    // タグ名を読む
+    let start_tag_name = many1::<String, _, _>(letter());
+    // (名前, 属性) で格納
+    let start_tag_content = (
+        start_tag_name,
+        // 空白/改行は読み飛ばす
         many::<String, _, _>(space().or(newline())),
+        // 属性を読む
         attributes(),
     )
         .map(|v: (String, _, AttrMap)| (v.0, v.2));
-    between(char('<'), char('>'), open_tag_content)
+
+    // パース
+    between(char('<'), char('>'), start_tag_content)
+}
+
+/** ====================================================
+ *   unit tests
+ * ====================================================
+ */
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_parse_start_tag() {
+        {
+            assert_eq!(
+                start_tag().easy_parse("<p>aaaa"),
+                Ok((("p".to_string(), AttrMap::new()), "aaaa"))
+            );
+        }
+        {
+            let mut attributes = AttrMap::new();
+            attributes.insert("id".to_string(), "test".to_string());
+            assert_eq!(
+                start_tag().easy_parse("<p id=\"test\">"),
+                Ok((("p".to_string(), attributes), ""))
+            )
+        }
+        {
+            let result = start_tag().easy_parse("<p id=\"test\" class=\"sample\">");
+            let mut attributes = AttrMap::new();
+            attributes.insert("id".to_string(), "test".to_string());
+            attributes.insert("class".to_string(), "sample".to_string());
+            assert_eq!(result, Ok((("p".to_string(), attributes), "")));
+        }
+
+        {
+            assert!(start_tag().easy_parse("<p id>").is_err());
+        }
+    }
 }
