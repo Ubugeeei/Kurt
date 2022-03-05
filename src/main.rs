b252e22d401002390458137dfaf186b88d25741b
Ubugeeei <ubuge1122@gamil.com>
2022-03-05 21:31:56 +0900

    feat: impl html parse playground

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,22 @@
-#[allow(unused_imports)]
-use combine::EasyParser;
+mod core;
 
-// mod core;
+use crate::core::html::parse_nodes;
 
 fn main() {
-//     let html_string = "\
-//   <html>\
-//     <head>\
-//       <title>my first html parse</title>\
-//     </head>\
-// \
-//     <body>\
-//       <div id=\"main\" class=\"content\">\
-//         <p>hello rust html parser!!</p>\
-//       </div>\
-//     </body>\
-//   </html>\
-//   ";
+    let html_string = "\
+  <html>\
+  <head>\
+      <title>my first html parse</title>\
+    </head>\
+\
+<body>\
+      <div id=\"main\" class=\"content\">\
+        <p>hello rust html parser!!</p>\
+      </div>\
+    </body>\
+  </html>\
+  ";
 
-//     let result = core::html::parse().easy_parse(html_string).ok().unwrap();
-//     dbg!(result);
+    let result = parse_nodes(html_string);
+    dbg!(result);
 }
