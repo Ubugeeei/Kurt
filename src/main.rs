5018ee415ed3c72409723d3884a0ef772a46986f
Ubugeeei <ubuge1122@gamil.com>
2022-03-07 01:33:46 +0900

    tweak: be html/css strings constant

--- src/main.rs
+++ src/main.rs
@@ -3,36 +3,37 @@ mod core;
 use crate::core::css::stylesheet::parse_css;
 use crate::core::html::parser::parse_html;
 
+const HTML: &str = r#"
+  <html>
+    <head>
+      <title>my first html parse</title>
+    </head>
+    <body>
+      <div id=\"main\" class=\"content\">
+        <p>hello rust html parser!!</p>
+      </div>
+    </body>
+  </html>
+"#;
+
+const CSS: &str = r#"
+  .content {
+    width: 1024px;
+    font-size: 12px;
+  }
+
+  p[id~=hello] {
+    font-size: 10px;
+    font-weight: bold;
+    color: grey;
+  }
+"#;
 fn main() {
-    let html_string = "\
-      <html>\
-        <head>\
-          <title>my first html parse</title>\
-        </head>\
-\
-        <body>\
-          <div id=\"main\" class=\"content\">\
-            <p>hello rust html parser!!</p>\
-          </div>\
-        </body>\
-      </html>\
-    ";
-
-    let css_string = "\
-      .content {\
-        width: 1024px;\
-        font-size: 12px;\
-      }\
-      p[id~=hello] {\
-        font-size: 10px;\
-      }\
-    ";
-
     // parse html
-    let dom = parse_html(html_string);
+    let dom = parse_html(HTML);
     dbg!(dom);
 
     // parse css
-    let cssom = parse_css(String::from(css_string));
+    let cssom = parse_css(String::from(CSS));
     dbg!(cssom);
 }
