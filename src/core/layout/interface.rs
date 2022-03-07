68e73c728dd5c0414e3c55b8c7fd19e3b54cede5
Ubugeeei <ubuge1122@gamil.com>
2022-03-08 01:12:08 +0900

    feat: impl layout interface

--- src/core/layout/interface.rs
+++ src/core/layout/interface.rs
@@ -1,15 +1,43 @@
 use crate::core::{NodeType, PropertyMap};
 
 #[derive(Debug, PartialEq)]
-pub struct LayoutBox<'a> {
-    pub box_type: BoxType<'a>,
-    pub children: Vec<LayoutBox<'a>>,
+pub struct LayoutDocument<'a> {
+    pub top_box: LayoutBox<'a>,
 }
 
 #[derive(Debug, PartialEq)]
-pub enum BoxType<'a> {
-    BlockBox(BoxProps<'a>),
-    InlineBox(BoxProps<'a>),
+pub struct LayoutBox<'a> {
+    pub box_type: BoxType,
+    pub box_props: Option<BoxProps<'a>>,
+    pub children: Vec<LayoutBox<'a>>,
+}
+impl<'a> LayoutBox<'a> {
+    pub fn inline_container(&mut self) -> &mut LayoutBox<'a> {
+        match self.box_type {
+            BoxType::InlineBox | BoxType::NoneBox | BoxType::AnonymousBox => self,
+            BoxType::BlockBox => {
+                match self.children.last() {
+                    Some(&LayoutBox {
+                        box_type: BoxType::AnonymousBox,
+                        ..
+                    }) => {}
+                    _ => self.children.push(LayoutBox {
+                        box_type: BoxType::AnonymousBox,
+                        box_props: None,
+                        children: vec![],
+                    }),
+                }
+                self.children.last_mut().unwrap()
+            }
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum BoxType {
+    BlockBox,
+    InlineBox,
+    NoneBox,
     AnonymousBox,
 }
 
