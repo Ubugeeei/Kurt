afedb37fdfd915dbf1df5025d6f4d82ad5c341aa
Ubugeeei <ubuge1122@gamil.com>
2022-03-08 01:09:36 +0900

    feat: impl create layout document

--- src/core/layout/create.rs
+++ src/core/layout/create.rs
@@ -1,45 +1,47 @@
-use crate::core::{StyledNode, Display};
+use crate::core::{Display, StyledDocument, StyledNode};
 
-use super::{LayoutBox, BoxType, BoxProps};
+use super::{BoxProps, BoxType, LayoutBox, LayoutDocument};
 
-pub fn create_layout_box<'a>(snode: StyledNode<'a>) -> LayoutBox<'a> {
-  LayoutBox {
-      box_type: match snode.display() {
-          Display::Block => BoxType::BlockBox(BoxProps {
-              node_type: snode.node_type,
-              properties: snode.properties,
-          }),
-          Display::Inline => BoxType::InlineBox(BoxProps {
-              node_type: snode.node_type,
-              properties: snode.properties,
-          }),
-          Display::None => unreachable!(),
-      },
-      children: snode
-          .children
-          .into_iter()
-          .fold(vec![], |mut acc: Vec<LayoutBox>, child| {
-              match child.display() {
-                  Display::Block => {
-                      acc.push(create_layout_box(child));
-                      acc
-                  }
-                  Display::Inline => {
-                      match acc.last() {
-                          Some(&LayoutBox {
-                              box_type: BoxType::AnonymousBox,
-                              ..
-                          }) => {}
-                          _ => acc.push(LayoutBox {
-                              box_type: BoxType::AnonymousBox,
-                              children: vec![],
-                          }),
-                      };
-                      acc.last_mut().unwrap().children.push(create_layout_box(child));
-                      acc
-                  }
-                  Display::None => unreachable!(),
-              }
-          }),
-  }
+pub fn create_layout_document<'a>(document: StyledDocument<'a>) -> LayoutDocument<'a> {
+    let layout_box = create_layout_box(document.document_element);
+
+    LayoutDocument {
+        top_box: layout_box,
+    }
+}
+
+fn create_layout_box<'a>(snode: StyledNode<'a>) -> LayoutBox<'a> {
+    let box_type = match snode.display() {
+        Display::Block => BoxType::BlockBox,
+        Display::Inline => BoxType::InlineBox,
+        Display::None => BoxType::NoneBox,
+    };
+
+    let box_props = BoxProps {
+        node_type: snode.node_type,
+        properties: snode.properties,
+    };
+
+    let mut layout = LayoutBox {
+        box_type: box_type,
+        box_props: Some(box_props),
+        children: vec![],
+    };
+
+    for child in snode.children {
+        match child.display() {
+            Display::Block => {
+                layout.children.push(create_layout_box(child));
+            }
+            Display::Inline => {
+                layout
+                    .inline_container()
+                    .children
+                    .push(create_layout_box(child));
+            }
+            Display::None => {}
+        }
+    }
+
+    layout
 }
