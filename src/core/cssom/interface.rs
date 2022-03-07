91e4a9d7312882487099084a98a20db745c81786
Ubugeeei <ubuge1122@gamil.com>
2022-03-07 22:56:39 +0900

    feat: impl selector matcher

--- src/core/cssom/interface.rs
+++ src/core/cssom/interface.rs
@@ -1,3 +1,5 @@
+use crate::core::{Node, NodeType};
+
 #[derive(Debug, PartialEq)]
 pub struct Stylesheet {
     pub rules: Vec<Rule>,
@@ -12,6 +14,11 @@ impl Stylesheet {
 pub struct Rule {
     pub selectors: Vec<Selector>, // a comma-separated list of selectors
     pub declarations: Vec<Declaration>,
+}
+impl Rule {
+    pub fn matches(&self, node: &Box<Node>) -> bool {
+        self.selectors.iter().any(|s| s.matches(node))
+    }
 }
 
 pub type Selector = SimpleSelector;
@@ -31,6 +38,50 @@ pub enum SimpleSelector {
     ClassSelector {
         class_name: String,
     },
+}
+impl SimpleSelector {
+    pub fn matches(&self, node: &Box<Node>) -> bool {
+        match self {
+            SimpleSelector::UniversalSelector => true,
+
+            SimpleSelector::TypeSelector { tag_name } => match node.node_type {
+                NodeType::Element(ref element) => element.tag_name.as_str() == tag_name,
+                _ => false,
+            },
+
+            SimpleSelector::AttributeSelector {
+                tag_name,
+                op,
+                attribute,
+                value,
+            } => match node.node_type {
+                NodeType::Element(ref element) => {
+                    element.tag_name.as_str() == tag_name
+                        && match op {
+                            AttributeSelectorOp::Eq => element.attributes.get(attribute) == Some(value),
+
+                            AttributeSelectorOp::Contain => element
+                                .attributes
+                                .get(attribute)
+                                .map(|value| {
+                                    value
+                                        .split_ascii_whitespace()
+                                        .find(|v| v == value)
+                                        .is_some()
+                                })
+                                .unwrap_or(false),
+                        }
+                }
+
+                _ => false,
+            },
+
+            SimpleSelector::ClassSelector { class_name } => match node.node_type {
+                NodeType::Element(ref element) => element.attributes.get("class") == Some(class_name),
+                _ => false,
+            },
+        }
+    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -56,4 +107,24 @@ pub enum Unit {
     Em,
     Px,
     Percent,
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::core::Element;
+
+    use super::*;
+
+    #[test]
+    fn test_universal_selector_behaviour() {
+        let element = &Element::new(
+            "p".to_string(),
+            [("id".to_string(), "test".to_string()),
+            ("class".to_string(), "testclass".to_string())]
+                .iter()
+                .cloned()
+                .collect(),
+            vec![],
+        );
+        assert_eq!(SimpleSelector::UniversalSelector.matches(element), true);
 }
