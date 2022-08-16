051985d1ff60ad5c3c288fb6814def8c8bbf6846
Ubugeeei <ubuge1122@gamil.com>
2022-08-16 20:25:09 +0900

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,37 @@
 mod core;
 
 use crate::core::{
-  css::stylesheet::parse_css,
-  html::parser::parse_html,
-  render::render::render,
-  runtime::JavaScriptRuntime,
-  {create_layout_document, create_styled_document},
+    css::stylesheet::parse_css,
+    html::parser::parse_html,
+    render::render::render,
+    runtime::JavaScriptRuntime,
+    {create_layout_document, create_styled_document},
 };
 
 extern crate sdl2;
 
-const HTML: &str = "\
-    <body>\
-      <div id=\"main\" class=\"content\">\
-        <div class=\"message\">Hello, world!! My browser is working!</div>\
-        <p class=\"hide\">this is hide element</p>\
-      </div>\
-      \
-      <script>\
-        let count = 6;\
-        const double = n => n * 2;\
-        double(count)\
-      </script>\
-      \
-      <script>\
-        const factorical = num => {\
-          if (num === 0) return 1;\
-          return num * factorical(num-1);\
+const HTML: &str = r#"
+    <body>
+      <div id="main" class="content">
+        <div class="message">Hello, world!! My browser is working!</div>
+        <p class="hide">this is hide element</p>
+      </div>
+
+      <script>
+        let count = 6;
+        const double = n => n * 2;
+        double(count)
+      </script>
+
+      <script>
+        const factorical = num => {
+          if (num === 0) return 1;
+          return num * factorical(num-1);
         };
         factorical(5)
-      </script>\
-    </body>\
-";
+      </script>
+    </body>
+"#;
 
 const CSS: &str = r#"
   body {
@@ -88,22 +88,22 @@ const DEFAULT_STYLESHEET: &str = r#"
 "#;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-  // parse html
-  let dom = parse_html(HTML).unwrap();
+    // parse html
+    let dom = parse_html(HTML).unwrap();
 
-  // parse css
-  let styles = format!("{}\n{}", DEFAULT_STYLESHEET, CSS);
-  let cssom = parse_css(styles).unwrap();
+    // parse css
+    let styles = format!("{}\n{}", DEFAULT_STYLESHEET, CSS);
+    let cssom = parse_css(styles).unwrap();
 
-  // create styled document from dom and cssom
-  let styled_document = create_styled_document(&dom, &cssom);
+    // create styled document from dom and cssom
+    let styled_document = create_styled_document(&dom, &cssom);
 
-  // calc layout
-  let layout_document = create_layout_document(styled_document);
+    // calc layout
+    let layout_document = create_layout_document(styled_document);
 
-  // create JavaScript runtime
-  let mut javascript_runtime = JavaScriptRuntime::new();
+    // create JavaScript runtime
+    let mut javascript_runtime = JavaScriptRuntime::new();
 
-  // TODO: rendering
-  render(&layout_document.top_box, &dom, &mut javascript_runtime)
+    // TODO: rendering
+    render(&layout_document.top_box, &dom, &mut javascript_runtime)
 }
